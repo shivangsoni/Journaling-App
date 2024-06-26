48f09bc40aff6533be4a087a3a61a098d42ed765
Shivang Soni <shivangsoni22@gmail.com>
2024-06-25 22:54:47 -0700

    on project load redirecting to localhost:5000

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from app import create_app
+import webbrowser
+import threading
 
 app = create_app()
 
+def open_browser():
+    webbrowser.open_new("http://localhost:5000")
+
 if __name__ == '__main__':
+    # Start a thread to open the browser after a short delay
+    threading.Timer(1, open_browser).start()
     app.run(debug=True)
